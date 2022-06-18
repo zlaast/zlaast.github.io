d287e380418cb8830d03a7b8909fa33add561e2b
Z'Laast Allicock <allicockz@gmail.com>
2022-06-18 10:46:07 -0400

    Updated Search

--- assets/js/database.js
+++ assets/js/database.js
@@ -1,4 +1,4 @@
-const url = 'http://192.168.2.210:8080';
+const url = 'https://zlaast.github.io';
 const DB = {
     q1: {
         CAT: "Coding",
@@ -8,7 +8,7 @@ const DB = {
         TITLE: "Project Euler",
         SUBTITLE: "Multiples of 3 or 5",
         AUTHOR: "Z'Laast Allicock",
-        DATE: "2022/06/20",
+        DATE: "2022/06/19",
         IURL: function () {
             return `/posts/${this.ID}/${this.TITLE} ${this.SUBID}/`;
         },
@@ -17,8 +17,34 @@ const DB = {
         },
         AUX: "",
         TAGS: [
+            "Coding",
             "Project Euler",
             "Python"
+        ]
+    },
+    q2: {
+        CAT: "Electronics",
+        ID: "Mains Alert",
+        SUBID: "1",
+        PART: "0",
+        TITLE: "Mains Alert - Part 1",
+        SUBTITLE: "Detecting 'the grid' vs a generator",
+        AUTHOR: "Z'Laast Allicock",
+        DATE: "2022/06/22",
+        IURL: function () {
+            return `/posts/${this.ID}/${this.ID} ${this.SUBID}/`;
+        },
+        EURL: function () {
+            return '';
+        },
+        AUX: "",
+        TAGS: [
+            "Electronics",
+            "Mains Alert",
+            "Mains (120V)",
+            "Arduino",
+            "ATTiny",
+            "C/C++"
         ]
     },
 };
