782aecb63eca16f3c9098781570850842288c367
Z'Laast Allicock <allicockz@gmail.com>
2024-03-02 02:01:56 -0500

    Minor updates

--- src/components/Svelte/Navigation.svelte
+++ src/components/Svelte/Navigation.svelte
@@ -19,17 +19,17 @@
 				<button aria-label="Scroll To Top Button" on:click={scrollToTop} class="mx-1 h-14 w-14 rounded-full bg-neutral-700 text-left text-neutral-500 duration-500 ease-in-out hover:bg-gray-700">
 					<span class="relative left-[10px] top-0.5 text-2xl text-neutral-200">
 						<svg xmlns="http://www.w3.org/2000/svg" class="h-9 w-9" fill="currentColor" viewBox="0 0 24 24">
-						  <path d="m6.293 13.293 1.414 1.414L12 10.414l4.293 4.293 1.414-1.414L12 7.586z"></path>
+							<path d="m6.293 13.293 1.414 1.414L12 10.414l4.293 4.293 1.414-1.414L12 7.586z"></path>
 						</svg>
-					  </span>
+					</span>
 				</button>
 
 				<!-- Search button -->
 				<button aria-label="Search Button" id="search-button" on:click={searchModalOpen} class="mx-1 h-14 w-14 rounded-full bg-neutral-700 text-left text-neutral-500 duration-500 ease-in-out hover:bg-gray-700">
 					<span class="relative left-4 top-0.5 text-2xl text-neutral-200">
-					  <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="currentColor" viewBox="0 0 24 24">
+						<svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="currentColor" viewBox="0 0 24 24">
 						<path d="M19.023 16.977a35.13 35.13 0 0 1-1.367-1.384c-.372-.378-.596-.653-.596-.653l-2.8-1.337A6.962 6.962 0 0 0 16 9c0-3.859-3.14-7-7-7S2 5.141 2 9s3.14 7 7 7c1.763 0 3.37-.66 4.603-1.739l1.337 2.8s.275.224.653.596c.387.363.896.854 1.384 1.367l1.358 1.392.604.646 2.121-2.121-.646-.604c-.379-.372-.885-.866-1.391-1.36zM9 14c-2.757 0-5-2.243-5-5s2.243-5 5-5 5 2.243 5 5-2.243 5-5 5z"></path>
-					  </svg>
+						</svg>
 					</span>
 				</button>
 
