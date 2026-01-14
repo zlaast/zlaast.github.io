24f7e39bc6922a6a0ef2d5ef683bf1df96b5eaf3
Z'Laast Allicock <allicockz@gmail.com>
2026-01-14 13:57:17 -0500

    Added two new posts

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,2 +1,24 @@
-// export const url = "http://localhost:4321"
-export const url = "https://zlaast.github.io"
+// This script is a centralized point for variables that I must manually change
+
+
+// URL ENDPOINT
+// export const url = "http://localhost:4321"          // Uncomment me when developing locally. Comment me out when deploying
+export const url = "https://zlaast.github.io"    // Uncomment me when ready to deploy. Comment me out when developing locally.
+
+
+// MENU PROGRESS BARS
+function percent(num: Number)
+{
+    return Math.round(Number(num) * 1000) / 10
+}
+
+export let project_progress = [
+    {
+        project: "Project Euler",
+        progress: percent(34/978)
+    },
+    {
+        project: "nand2tetris",
+        progress: percent(5/14)
+    }
+]
